8fc2532e7003c240124f5866162cb21e13f2e854
Vadim <vadlen2149@gmail.com>
2024-03-19 12:30:51 +0300

    fix CorrectWordPipe

--- src/app/core/pipes/correct-word.pipe.ts
+++ src/app/core/pipes/correct-word.pipe.ts
@@ -7,12 +7,18 @@ export class CorrectWordPipe implements PipeTransform {
 
   transform(value: number | undefined): unknown {
     switch (value) {
-      case 1 || 21:
-        return ' сезон';
-      case 2 || 3 || 4 || 22 || 23 || 24:
-        return ' сезона';
+      case 1:
+      case 21:
+        return value + ' сезон';
+      case 2:
+      case 3:
+      case 4:
+      case 22:
+      case 23:
+      case 24:
+        return value + ' сезона';
       default:
-        return ' сезонов';
+        return value + ' сезонов';
     }
   }
 
